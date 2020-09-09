5dd593ca97d92e7ad4944b4db4e00df22fa8a532
Володимир Зонтов <vladimir.zontov@gmail.com>
2020-09-09 17:20:26 +0300

    fix Game 15

--- lesson13_15.py
+++ lesson13_15.py
@@ -31,29 +31,29 @@ class My15:
                         col_ = j
         return row_, col_
 
-    def move_up(self):
+    def move_down(self):
         if self.row == 3:
             return
         self.my_15[self.row][self.col], self.my_15[self.row + 1][self.col] = self.my_15[self.row + 1][self.col], \
                                                                              self.my_15[self.row][self.col]
         self.row += 1
 
-    def move_down(self):
+    def move_up(self):
         if self.row == 0:
             return
         self.my_15[self.row][self.col], self.my_15[self.row - 1][self.col] = self.my_15[self.row - 1][self.col], \
                                                                              self.my_15[self.row][self.col]
         self.row -= 1
 
-    def move_left(self):
+    def move_right(self):
         if self.col == 3:
             return
         self.my_15[self.row][self.col], self.my_15[self.row][self.col + 1] = self.my_15[self.row][self.col + 1], \
                                                                              self.my_15[self.row][self.col]
         self.col += 1
 
-    def move_right(self):
-        if self.row == 0:
+    def move_left(self):
+        if self.col == 0:
             return
         self.my_15[self.row][self.col], self.my_15[self.row][self.col - 1] = self.my_15[self.row][self.col - 1], \
                                                                              self.my_15[self.row][self.col]
@@ -63,28 +63,29 @@ class My15:
 def draw_15_table(my_15):
     for row_index, row in enumerate(my_15.my_15):
         for col_index, col in enumerate(row):
-            label = Entry(root, width=2, fg='blue', font=('Arial', 50, 'bold'), justify='center')
+            label = Entry(root, width=2, fg='white', bg='black', font=('Arial', 50, 'bold'), justify='center')
+            label.config(highlightbackground = "black")
             label.grid(row=row_index, column=col_index)
             label.insert(END, col)
 
 
 def left(event):
-    my_15.move_right()
-    draw_15_table(my_15)
-
-
-def right(event):
     my_15.move_left()
     draw_15_table(my_15)
 
 
+def right(event):
+    my_15.move_right()
+    draw_15_table(my_15)
+
+
 def up(event):
-    my_15.move_down()
+    my_15.move_up()
     draw_15_table(my_15)
 
 
 def down(event):
-    my_15.move_up()
+    my_15.move_down()
     draw_15_table(my_15)
 
 
@@ -92,6 +93,7 @@ my_15 = My15()
 root = Tk()
 root.title("15")
 root.geometry("275x280")
+root.configure(background='black')
 draw_15_table(my_15)
 root.bind("<Left>", left)
 root.bind("<Right>", right)
